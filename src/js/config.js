761949ab84f53f37b779b2daf1db4581ad2002c6
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-08-30 23:59:34 +0800

    added forkify to GitHub

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "4faa3bfb-875e-4f6f-9fc7-7d7bb11c045d";
+export const MODAL_CLOSE_SEC = 2.5;
